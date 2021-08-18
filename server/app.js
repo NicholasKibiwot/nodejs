e1b28ac0d272f817a314701ae354e28479c8c99a
nicscott254 <nicolous.kibiwot@gmail.com>
2021-08-18 18:46:09 +0300

    first commit

---
+++ server/app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const PORT = 5000
+const {MONGOURI} = require('./keys')
+
+mongoose.connect(MONGOURI,{
+  
+useNewUrlParser: true,
+useUnifiedTopology: true 
+
+})
+
+mongoose.connection.on('connected',()=>{
+    console.log("connected to mongo")
+})
+
+mongoose.connection.on('error',(err)=>{
+    console.log("err connecting",err)
+    })
+
+
+    app.listen(PORT,()=>{
+        console.log("server running on",PORT)}
+    )
---
+++ server/keys.js
@@ -0,0 +1,4 @@
+module.exports={
+   MONGOURI:"mongodb+srv://nickscot:90fMWKdQQjKbc937@cluster0.f6kxb.mongodb.net/NODEJS?retryWrites=true&w=majority"
+}
+
