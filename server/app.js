486dc50c013eaeb4dd08052e87a04cf31e4e2276
nicscott254 <nicolous.kibiwot@gnail.com>
2021-08-20 16:02:06 +0300

    initial commit

--- server/app.js
+++ server/app.js
@@ -4,6 +4,10 @@ const mongoose = require('mongoose')
 const PORT = 5000
 const {MONGOURI} = require('./keys')
 
+require('./models/user')
+app.use(express.json())
+app.use(require('./routes/auth'))
+app.use(express.json())
 mongoose.connect(MONGOURI,{
   
 useNewUrlParser: true,
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,8 +1,36 @@
 const express = require('express')
 const router = express.Router()
+mongoose =require('mongoose')
+const User =mongoose.model("User")
 
-router.get('/',req,res =>{
+router.get('/',(req,res) =>{
     res.send("Hello")
+})
+
+router.post('/signup',(req,res) =>{
+    const{name,email,password} = req.body
+    if(!name || !password || !email){
+     return res.status(422).json({errors:"please add all the fields"})
+    }
+    User.findOne({email:email})
+    .then((savedUser) => {
+        if(savedUser){
+            return res.status(422).json({errors:"user already exists with that email"})
+        }
+        const user = new User({email,
+        password,
+        name})
+        user.save()
+        .then(user => {
+            res.json({message:"saved succesfully"})
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+    })
+    .catch(err => {
+        console.log(err)
+    })
 })
 
 
